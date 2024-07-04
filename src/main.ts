49479ca782840871f690a2244b40f543427f090e
happymeex <32947316+happymeex@users.noreply.github.com>
2024-07-03 22:46:21 -0700

    message parsing

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { getScrollableAndMessageContainer } from "./scraper";
 import { scrollUp } from "./utils";
-import { getAllText } from "./messageParser";
+import { getMessageContent } from "./messageParser";
 function main() {
   const { scrollContainer, messageDiv } = getScrollableAndMessageContainer();
   console.log(scrollContainer);
   scrollUp(scrollContainer);
   console.log(messageDiv);
   for (let message of Array.from(messageDiv.children)) {
-    const allText = getAllText(message);
+    const allText = getMessageContent(message);
     console.log(allText);
   }
 }
--- src/messageParser.ts
+++ src/messageParser.ts
@@ -6,6 +6,7 @@ enum TextType {
   ENTER,
   TIME,
   UNSPECIFIED,
+  SENT_MARKER,
 }
 
 type TextLabel = {
@@ -13,13 +14,96 @@ type TextLabel = {
   text: string;
 };
 
-export function getAllText(node: Node): TextLabel[] {
-  const textLabels = getAllTextIncompleteLabels(node);
-  // TODO: Parse the rest of the labels
-  return textLabels;
+type ReplyInfo = {
+  /** Name of person replied to */
+  addresseeName: string;
+  /** Text of message replied to (or preview thereof) */
+  originalMessage: string;
+};
+type Message = {
+  time: string | null;
+  /** Info if this message was a reply, null if not */
+  replyInfo: ReplyInfo | null;
+  senderName: string;
+  body: string;
+  isImage: boolean;
+};
+
+/**
+ * Retrieves the message content from a given Node.
+ *
+ * @param node the Node containing the message content.
+ * @return the extracted message content or null if there was an error.
+ */
+export function getMessageContent(node: Node): Message | null {
+  const textLabels = getMessageTextIncompleteLabels(node);
+
+  if (textLabels.length === 0) return null;
+
+  if (textLabels.length === 1) {
+    const senderName = textLabels[0].text;
+    return {
+      time: null,
+      replyInfo: null,
+      senderName: senderName === "You sent" ? "You" : senderName,
+      body: "",
+      isImage: true,
+    };
+  }
+
+  const messageBody = textLabels[textLabels.length - 1].text;
+  textLabels.pop();
+
+  // check if it's a reply
+  const repliedTo = textLabels.findIndex(
+    (label) => label.type === TextType.REPLY_INFO
+  );
+  let senderName: string | null = null;
+  let replyInfo: ReplyInfo | null = null;
+  if (repliedTo === 0) return null; // reply missing sender/addressee info
+  if (repliedTo > 0) {
+    let addresseeName: string | null = null;
+    // regex to extract the sender and addressee
+    const rx = RegExp("^(.*) replied to (.*)$");
+    const match = textLabels[repliedTo - 1].text.match(rx);
+    if (match) {
+      senderName = match[1];
+      addresseeName = match[2];
+    }
+    if (addresseeName === null || senderName === null) {
+      return null;
+    }
+    const originalMessage = textLabels
+      .splice(repliedTo + 1)
+      .map((label) => label.text)
+      .join("");
+
+    replyInfo = {
+      addresseeName,
+      originalMessage,
+    };
+  } else {
+    if (textLabels.length === 0) return null;
+    senderName = textLabels[textLabels.length - 1].text;
+    senderName = senderName === "You sent" ? "You" : senderName;
+    textLabels.pop();
+  }
+
+  let time = null;
+  if (textLabels.length !== 0) {
+    time = textLabels[textLabels.length - 1].text;
+  }
+
+  return {
+    time,
+    replyInfo,
+    senderName,
+    body: messageBody,
+    isImage: false,
+  };
 }
 
-function getAllTextIncompleteLabels(node: Node): TextLabel[] {
+function getMessageTextIncompleteLabels(node: Node): TextLabel[] {
   if (node instanceof Text) {
     const parentElt = node.parentElement;
     let textType: TextType = TextType.UNSPECIFIED;
@@ -33,14 +117,24 @@ function getAllTextIncompleteLabels(node: Node): TextLabel[] {
         textType = TextType.ENTER;
       } else if (node.data === "Original message:") {
         textType = TextType.REPLY_INFO;
+      } else if (
+        node.data.startsWith("Sent") &&
+        parentElt instanceof HTMLSpanElement
+      ) {
+        textType = TextType.SENT_MARKER;
       }
     }
 
-    if (textType === TextType.ENTER) return [];
+    if (
+      textType === TextType.ENTER ||
+      textType === TextType.REACT_COUNT ||
+      textType === TextType.SENT_MARKER
+    )
+      return [];
     return [{ type: textType, text: node.data }];
   }
   if (node instanceof Element) {
-    return Array.from(node.childNodes).flatMap(getAllText);
+    return Array.from(node.childNodes).flatMap(getMessageTextIncompleteLabels);
   }
   return [];
 }
