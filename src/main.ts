5d9bb6f067cde76d452fcb70d2e9e0999e244161
happymeex <32947316+happymeex@users.noreply.github.com>
2024-07-04 12:57:20 -0700

    basic ui panel for starting and stopping scrape

--- src/main.ts
+++ src/main.ts
@@ -99,60 +99,76 @@ function scrapeCurrentChat() {
   const processedDivs = new Set<Element>();
   let currentDivToProcess = messageDiv.lastElementChild;
 
-  // Returns true if should terminate, otherwise false
-  const tryMovingToPreviousSiblingMessageDiv = () => {
-    if (currentDivToProcess === null) {
-      return true;
-    }
-    const prevSibling = currentDivToProcess.previousElementSibling;
-    if (prevSibling === null) {
-      return true;
-    } else if (!isMessageDiv(prevSibling)) {
-      if (isProfileBanner(prevSibling)) {
+  let scrapeProcess: number | null = null;
+  const startScrape = () => {
+    console.log("Scraping...");
+    // Returns true if should terminate, otherwise false
+    const tryMovingToPreviousSiblingMessageDiv = () => {
+      if (currentDivToProcess === null) {
         return true;
       }
-      prevSibling.scrollIntoView(false);
-      return false;
-    } else {
-      currentDivToProcess = prevSibling;
-      return false;
-    }
+      const prevSibling = currentDivToProcess.previousElementSibling;
+      if (prevSibling === null) {
+        return true;
+      } else if (!isMessageDiv(prevSibling)) {
+        if (isProfileBanner(prevSibling)) {
+          return true;
+        }
+        prevSibling.scrollIntoView(false);
+        return false;
+      } else {
+        currentDivToProcess = prevSibling;
+        return false;
+      }
+    };
+
+    // Returns true if should terminate, otherwise false
+    const processLastMessage = () => {
+      if (currentDivToProcess === null) {
+        return true;
+      }
+      const [message, status] = getMessageContent(currentDivToProcess);
+      if (
+        status === MessageParseStatus.NOT_A_MESSAGE ||
+        status === MessageParseStatus.TERMINATE
+      ) {
+        return true;
+      }
+      if (
+        status === MessageParseStatus.EMPTY_MESSAGE ||
+        processedDivs.has(currentDivToProcess)
+      ) {
+        if (tryMovingToPreviousSiblingMessageDiv()) return true;
+        return false;
+      } else {
+        processedMessages.push(message);
+        processedDivs.add(currentDivToProcess);
+        if (tryMovingToPreviousSiblingMessageDiv()) return true;
+        return false;
+      }
+    };
+
+    const process = setInterval(() => {
+      if (processLastMessage() || window.innerWidth < 768) {
+        console.log(processedMessages);
+        writeToNewWindow(JSON.stringify(processedMessages));
+        console.log("Done!");
+        clearInterval(process);
+      }
+    }, POLLING_TIME);
+    scrapeProcess = process;
   };
 
-  // Returns true if should terminate, otherwise false
-  const processLastMessage = () => {
-    if (currentDivToProcess === null) {
-      return true;
-    }
-    const [message, status] = getMessageContent(currentDivToProcess);
-    if (
-      status === MessageParseStatus.NOT_A_MESSAGE ||
-      status === MessageParseStatus.TERMINATE
-    ) {
-      return true;
-    }
-    if (
-      status === MessageParseStatus.EMPTY_MESSAGE ||
-      processedDivs.has(currentDivToProcess)
-    ) {
-      if (tryMovingToPreviousSiblingMessageDiv()) return true;
-      return false;
-    } else {
-      processedMessages.push(message);
-      processedDivs.add(currentDivToProcess);
-      if (tryMovingToPreviousSiblingMessageDiv()) return true;
-      return false;
-    }
-  };
-
-  const process = setInterval(() => {
-    if (processLastMessage() || window.innerWidth < 768) {
-      console.log(processedMessages);
+  const stopScrape = () => {
+    if (scrapeProcess) {
       writeToNewWindow(JSON.stringify(processedMessages));
-      console.log("Done!");
-      clearInterval(process);
+      console.log("Stopped scraping!");
+      clearInterval(scrapeProcess);
+      scrapeProcess = null;
     }
-  }, POLLING_TIME);
+  };
+
+  return { startScrape, stopScrape };
 }
 
 main();
