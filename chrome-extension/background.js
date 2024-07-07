5e34b4ff8e5d47995d3b4c925a69d2c9fe44b2c3
happymeex <32947316+happymeex@users.noreply.github.com>
2024-07-07 02:15:44 -0700

    remove activeTab permission

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,5 +1,13 @@
 chrome.action.onClicked.addListener((tab) => {
-  if (tab.url.startsWith("https://www.messenger.com/")) {
-    chrome.tabs.sendMessage(tab.id, { message: "chat-scraper-init" });
-  }
+  chrome.tabs.sendMessage(tab.id, { message: "chat-scraper-init" }, (res) => {
+    const lastError = chrome.runtime.lastError;
+    // Ignore errors from trying to trigger the scraper on a non-Messenger tab
+    if (
+      !lastError?.message
+        ?.toLowerCase()
+        .includes("receiving end does not exist")
+    ) {
+      console.error(error);
+    }
+  });
 });
